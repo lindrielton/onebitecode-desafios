966b6e1dd918b6a49a1579364d4d2fbfbe9d305d
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2026-01-14 22:42:43 -0300

    parte 1

--- desafios/ex15/ex15.js
+++ desafios/ex15/ex15.js
@@ -15,7 +15,74 @@ function listarVagas(){
 
 
 function novaVaga(){
-    const nome = prompt
+    const nome = prompt('Informe um nome para a vaga')
+    const descricao = prompt('Informe uma descrição para a vaga:')
+    const dataLimite = prompt('informe uma data limite (dd/mm/aaa) para a vaga:')
+
+    const comfirmacao = confirm(
+        "Deseja criar uma nova vaga com essas informaçoes?\n" +
+        "Nome:" + nome + "\nDescrição" + descricao + "\nData Limite: " + dataLimite
+    )
+
+    if(comfirmacao){
+        const novaVaga = {nome, descricao, dataLimite, candidatos:[]}
+        vagas.push(novaVaga)
+        alert("vaga criada.")
+
+    }
+}
+
+function exibirVaga(){
+    const indice = prompt("informe o indice da vaga que desejá exibir:")
+    const vaga = vagas[indice]
+
+    const vagasEmtexto = vaga.reduce(function (acumulador, candidato){
+        return acumulador + '\n-' + candidato
+
+    }, " ")
+
+    alert(
+        "\nVaga n°: " + indice + 
+        "\nNome :" + vaga.nome +
+        "\nDescrição : " + vaga.descricao +
+        "\nData limite: " + vaga.dataLimite +
+        "\nCandidatos: "  + vaga.candidatos.length +
+        "\nCandidatos Inscritos :" + vagasEmtexto
+
+    )
+}
+
+function inscreverCandidato(){
+     const candidato = prompt('Informe o nome  do (a) candidato(a):')
+     const indice = prompt("Informe o Indice da vaga para a qual o(a) candidato deseja se inscrever")
+     const vaga = vagas[indice]
+
+     const confirmacao = confirm(
+        'Desejá increver este candidato' + candidatos + ' na vaga ' + indice + "?\n" +
+        "Nome :" + vaga.Nome + "\nDescrição" + vaga.descricao + "\nData Limite: " + vaga.dataLimite
+
+     )
+
+     if(confirmacao){
+        alert('inscrição criada')
+     }
+
+
+}
+
+
+function excluirVaga(){
+    const indice = prompt("Informe o indece que voce desejã excluir")
+    const vaga = vagas[indice]
+
+    const confirmacao = confirm('tem certeza que desejá excluir a vaga ' + indice + "?\n" +
+        "Nome :" + vaga.Nome + "\nDescrição" + vaga.descricao + "\nData Limite: " + vaga.dataLimite
+     )
+
+    if(confirmacao){
+        vaga.splice(indice, 1)
+        alert('vaga excluida')
+    }
 }
 
 
